d61f872b98c5e804863ddc86ffdee18bce0168a5
wangxiaobo <2078350663@qq.com>
2020-03-04 12:16:49 +0800

    parking  model  及验证

---
+++ db/migrate/20200304040919_create_parkings.rb
@@ -0,0 +1,13 @@
+class CreateParkings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :parkings do |t|
+      t.string :parking_type
+      t.datetime :start_at
+      t.datetime :end_at
+      t.integer :amount
+      t.user :belongs_to
+
+      t.timestamps
+    end
+  end
+end
